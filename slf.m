1bd681d6dc0c73b8beb30fce3f410f60dae7714e
Yajun Bai <38762233+AUSB@users.noreply.github.com>
2020-02-14 23:55:47 -0500

    Add files via upload

---
+++ slf.m
@@ -0,0 +1,15 @@
+%SLF Summary of this function goes here
+%   Detailed explanation goes here
+%% Square loss function
+function SLF = slf(fct, data)
+    % Input fct, function, function to perform
+    % Input data, Matrix<double>, f(X) = Y data points
+    % Output SLF, double, Sum of square losses
+    X = data(:,1); Y = data(:,2);
+    SLF = 0;
+    for i=1:length(X)
+        % subs : substitute x with value in the function
+        SLF = SLF + (subs(fct,X(i,1))- Y(i,1))^2;
+    end
+    SLF = 1/2*SLF;
+end
